573498f568b993ed323318834df6ac947ed97284
sauzzy23 <ashapatle22@gmail.com>
2023-11-07 16:57:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import WordCounter from './WordCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Word Counter</h1>
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
